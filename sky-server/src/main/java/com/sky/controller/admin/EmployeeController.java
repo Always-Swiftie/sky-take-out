6f5c46f05ea1207c8bb829c06dfdd9696d25f8d9
Always-Swiftie <mebuster06@gmail.com>
2025-06-30 19:09:09 +0800

    day2-新增员工分页查询接口

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,20 +1,20 @@
 package com.sky.controller.admin;
 
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -62,9 +62,9 @@ public class EmployeeController {
         return Result.success(employeeLoginVO);
     }
 
-    /*
-      新增员工
-      @param employeeDTO
+    /**
+      *新增员工
+      *@param employeeDTO
      * @return
      */
 
@@ -73,6 +73,19 @@ public class EmployeeController {
         log.info("新增员工: {}", employeeDTO);
         employeeService.save(employeeDTO);
         return Result.success();
+    }
+
+    /**
+     *员工分页查询
+     * @param employeePageQueryDTO
+     * @return PageResult
+
+     */
+    @GetMapping("/page")
+    public Result page(EmployeePageQueryDTO employeePageQueryDTO) {
+        log.info("分页查询: {}", employeePageQueryDTO);
+        PageResult page = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(page);
     }
 
 
