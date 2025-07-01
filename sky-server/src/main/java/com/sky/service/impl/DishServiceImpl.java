97e17c9ebdc3378a99a773c655041239032552f1
Always-Swiftie <mebuster06@gmail.com>
2025-07-01 13:09:58 +0800

    day3-新增菜品添加接口

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,55 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        //首先需要把DTO对象转换成一个实体Dish
+        //注意DTO对象中有Flavor属性，但是Dish对象中没有
+        //那么这里涉及两步操作：1.向dish表中插入1条菜品数据（不包含Flavor属性）
+        //2.向Flavor表中插入若干条数据，具体取决于DTO中Flavor这个属性
+        log.info("新增菜品:{}",dishDTO);
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //step1.向dish表插入1条数据
+        dishMapper.insert(dish);
+
+        //step2.获取DTO对象中的Flavor属性
+        Long dishId = dish.getId();
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && !flavors.isEmpty()){
+            for(DishFlavor flavor : flavors){
+                flavor.setDishId(dishId);
+            }
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+    }
+}
